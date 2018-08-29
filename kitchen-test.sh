9b2350235db9dda109c2968305130a72f16d75eb
Daniel DeGroff <daniel@inversoft.com>
2018-08-29 17:14:57 -0600

    working on the testing harness

--- kitchen-test.sh
+++ kitchen-test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Assume a sibling project is checked out next to us called fusionauth-install
+if [ ! -d ../fusionauth-install ]; then
+  echo "It looks like you do not have fusionauth-install checked out, we'll do that for you"
+  cd ../
+  git clone https://github.com/FusionAuth/fusionauth-install.git
+  cd ./fusionauth-install-testing
+fi
 
+echo "Copy the install.sh from the fusionauth-install project"
 cp ../fusionauth-install/install.sh .
 bundle exec kitchen test
