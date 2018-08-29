ef8cbf6d1d724050d8594ba1fcf2d1db82901326
Daniel DeGroff <daniel@inversoft.com>
2018-08-29 17:23:50 -0600

    README and comments

---
+++ README.md
@@ -0,0 +1,17 @@
+# FusionAuth Install Testing
+
+This project is set up to test the FusionAuth install scripts. 
+
+### Setup
+To get the project set up and ready for testing, run the following command:
+```
+> bundle install
+```
+
+
+### Testing
+To execute the test on Linux or macOS, run the following command:
+
+```
+> ./kitchen-test.sh
+```
--- kitchen-test.sh
+++ kitchen-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Start the Kitchen testing!!
+
 # Assume a sibling project is checked out next to us called fusionauth-install
 if [ ! -d ../fusionauth-install ]; then
   echo "It looks like you do not have fusionauth-install checked out, we'll do that for you"
@@ -8,6 +10,11 @@ if [ ! -d ../fusionauth-install ]; then
   cd ./fusionauth-install-testing
 fi
 
-echo "Copy the install.sh from the fusionauth-install project"
-cp ../fusionauth-install/install.sh .
+if [ -f ./install.sh ]; then
+  echo "You already have a copy of install.sh in the current working directory, leaving it alone."
+else
+  echo "Copy the install.sh from the fusionauth-install project"
+  cp ../fusionauth-install/install.sh .
+fi
+
 bundle exec kitchen test
